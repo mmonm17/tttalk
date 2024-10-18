692430ca64a4e430cc072ce80b3960744e50b373
fir3yice (ceo) <camron.ong@gmail.com>
2024-10-18 17:49:16 +0800

    added flag icons for level views

--- app/src/main/java/com/t_t_talk/LevelsTagalogActivity.java
+++ app/src/main/java/com/t_t_talk/LevelsTagalogActivity.java
@@ -48,13 +48,14 @@ public class LevelsTagalogActivity extends AppCompatActivity {
         setupRecyclerView();
         getSupportFragmentManager()
                 .beginTransaction()
+                .replace(R.id.fragment_flag_ta, new FlagIconFragment(R.drawable.img_flag_ph))
+                .commit();
+        getSupportFragmentManager()
+                .beginTransaction()
                 .replace(R.id.fragment_navigation_panel, new NavigationPanelFragment(true, false))
                 .commit();
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_flag_ta, new FlagIconFragment(R.drawable.img_flag_ph))
-                .commit();
+
     }
 
     private void setupRecyclerView() {
